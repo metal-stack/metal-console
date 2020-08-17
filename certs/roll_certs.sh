f83675ca540ec9a1d72277b3eac17d4b3be42e56
Sandro Koll <sandro.koll@f-i-ts.de>
2020-08-17 13:41:53 +0200

    ca cert has been expired

--- certs/roll_certs.sh
+++ certs/roll_certs.sh
@@ -6,3 +6,4 @@ rm -f *.pem
 cfssl gencert -ca=../ca.pem -ca-key=../ca-key.pem -config=../ca-config.json -profile=server server.json | cfssljson -bare server
 cfssl gencert -ca=../ca.pem -ca-key=../ca-key.pem -config=../ca-config.json -profile=client client.json | cfssljson -bare client
 rm *.csr
+./test_certs.sh --client-pem=client.pem --client-key=client-key.pem --server-pem=server.pem --server-key=server-key.pem --host=metal-console
