0240b0272cea3aea209cb1dcf86fd07f52e4352a
Stefan Majer <stefan.majer@gmail.com>
2021-07-09 09:06:41 +0200

    Better version output (#17)

--- cmd/bmcproxy/main.go
+++ cmd/bmcproxy/main.go
@@ -1,43 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/kelseyhightower/envconfig"
 	"github.com/metal-stack/metal-console/internal/bmcproxy"
-	"github.com/metal-stack/metal-lib/zapup"
-	"os"
-)
-
-var (
-	version   = "devel"
-	revision  string
-	gitsha1   string
-	builddate string
+	"github.com/metal-stack/v"
+	"go.uber.org/zap"
 )
 
 func main() {
 	spec := &bmcproxy.Specification{}
 	err := envconfig.Process("BMC_PROXY", spec)
-	logger := zapup.MustRootLogger().Sugar()
+	logger, _ := zap.NewProduction()
+	sugar := logger.Sugar()
 	if err != nil {
-		logger.Errorw("failed to read env config", "error", err)
+		sugar.Errorw("failed to read env config", "error", err)
 		os.Exit(1)
 	}
 
-	logger.Infow("bmc-proxy", "version", getVersionString(), "port", spec.Port)
+	sugar.Infow("bmc-proxy", "version", v.V, "port", spec.Port)
 
-	bmcproxy.New(logger, spec).Run()
-}
-
-func getVersionString() string {
-	var versionString = version
-	if gitsha1 != "" {
-		versionString += " (" + gitsha1 + ")"
-	}
-	if revision != "" {
-		versionString += ", " + revision
-	}
-	if builddate != "" {
-		versionString += ", " + builddate
-	}
-	return versionString
+	bmcproxy.New(sugar, spec).Run()
 }
--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,50 +1,31 @@
 package main
 
 import (
-	"github.com/metal-stack/metal-console/internal/console"
 	"os"
 
-	"github.com/kelseyhightower/envconfig"
-	"github.com/metal-stack/metal-lib/zapup"
-)
+	"github.com/metal-stack/metal-console/internal/console"
+	"go.uber.org/zap"
 
-var (
-	version   = "devel"
-	revision  string
-	gitsha1   string
-	builddate string
+	"github.com/kelseyhightower/envconfig"
+	"github.com/metal-stack/v"
 )
 
 func main() {
 	spec := &console.Specification{}
 	err := envconfig.Process("METAL_CONSOLE", spec)
-	logger := zapup.MustRootLogger().Sugar()
+	logger, _ := zap.NewProduction()
+	sugar := logger.Sugar()
 	if err != nil {
-		logger.Errorw("failed to read env config", "error", err)
+		sugar.Errorw("failed to read env config", "error", err)
 		os.Exit(1)
 	}
 
-	logger.Infow("metal-console", "version", getVersionString(),
-		"port", spec.Port, "metal-api", spec.MetalAPIURL, "devmode", spec.DevMode())
+	sugar.Infow("metal-console", "version", v.V, "port", spec.Port, "metal-api", spec.MetalAPIURL, "devmode", spec.DevMode())
 
-	s, err := console.NewServer(logger, spec)
+	s, err := console.NewServer(sugar, spec)
 	if err != nil {
-		logger.Errorw("failed to create metal-go driver", "error", err)
+		sugar.Errorw("failed to create metal-go driver", "error", err)
 		os.Exit(1)
 	}
 	s.Run()
-}
-
-func getVersionString() string {
-	var versionString = version
-	if gitsha1 != "" {
-		versionString += " (" + gitsha1 + ")"
-	}
-	if revision != "" {
-		versionString += ", " + revision
-	}
-	if builddate != "" {
-		versionString += ", " + builddate
-	}
-	return versionString
 }
