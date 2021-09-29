2ca4d02638c4a96fbf5a5b61a381e276473460bc
Stefan Majer <stefan.majer@f-i-ts.de>
2021-09-29 08:20:15 +0200

    set idle timeout

--- internal/console/consoleServer.go
+++ internal/console/consoleServer.go
@@ -56,6 +56,7 @@ func (cs *consoleServer) Run() {
 		Addr:             fmt.Sprintf(":%d", cs.spec.Port),
 		Handler:          cs.sessionHandler,
 		PublicKeyHandler: cs.authHandler,
+		IdleTimeout:      time.Minute * 10,
 	}
 
 	hostKey, err := loadHostKey()
