b12a93ba0dad6d89f980c9cc0acc19296d59fc9b
Markus Wennrich <mwennrich@gmail.com>
2022-06-21 10:30:07 +0200

    update to metal-go client interface (#30)

--- internal/console/machineClient.go
+++ internal/console/machineClient.go
@@ -1,41 +1,31 @@
 package console
 
 import (
-	"fmt"
-
 	metalgo "github.com/metal-stack/metal-go"
+	"github.com/metal-stack/metal-go/api/client/machine"
 	"github.com/metal-stack/metal-go/api/models"
 )
 
-func newClient(metalAPIURL string, hmac string) (*metalgo.Driver, error) {
-	driver, err := metalgo.NewDriver(metalAPIURL, "", hmac)
+func newClient(metalAPIURL string, hmac string) (metalgo.Client, error) {
+	client, _, err := metalgo.NewDriver(metalAPIURL, "", hmac)
 	if err != nil {
 		return nil, err
 	}
-	return driver, nil
+	return client, nil
 }
 
 func (cs *consoleServer) getMachine(machineID string) (*models.V1MachineResponse, error) {
-	mfr := &metalgo.MachineFindRequest{
-		ID: &machineID,
-	}
-	resp, err := cs.client.MachineFind(mfr)
+	resp, err := cs.client.Machine().FindMachine(machine.NewFindMachineParams().WithID(machineID), nil)
 	if err != nil {
 		return nil, err
 	}
-	if len(resp.Machines) == 0 {
-		return nil, fmt.Errorf("no machine found with ID %q", machineID)
-	}
-	if len(resp.Machines) > 1 {
-		return nil, fmt.Errorf("%d machines found with ID %q", len(resp.Machines), machineID)
-	}
-	return resp.Machines[0], nil
+	return resp.Payload, nil
 }
 
 func (cs *consoleServer) getMachineIPMI(machineID string) (*models.V1MachineIPMIResponse, error) {
-	resp, err := cs.client.MachineIPMIGet(machineID)
+	resp, err := cs.client.Machine().FindIPMIMachine(machine.NewFindIPMIMachineParams().WithID(machineID), nil)
 	if err != nil {
 		return nil, err
 	}
-	return resp.Machine, nil
+	return resp.Payload, nil
 }
