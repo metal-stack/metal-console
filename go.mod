121f89877cc2fbec4da3e86ef4f40448befa8fce
Stefan Majer <stefan.majer@f-i-ts.de>
2026-02-22 09:52:59 +0100

    component ping

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.26
 require (
 	github.com/gliderlabs/ssh v0.3.8
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/metal-stack/api v0.0.48
+	github.com/metal-stack/api v0.0.51
 	github.com/metal-stack/metal-lib v0.23.5
 	github.com/metal-stack/v v1.0.3
 	github.com/stretchr/testify v1.11.1
@@ -18,6 +18,7 @@ require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.4 // indirect
 	github.com/klauspost/connect-compress/v2 v2.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+	"time"
 
 	apiclient "github.com/metal-stack/api/go/client"
+	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
 	"github.com/metal-stack/metal-console/internal/console"
 
 	"github.com/kelseyhightower/envconfig"
@@ -32,6 +35,18 @@ func main() {
 		log.Error("failed to create metal client", "error", err)
 		panic(err)
 	}
+
+	// Ping apiserver every 5min
+	client.Ping(context.Background(), &apiclient.PingConfig{
+		ComponentType: apiv2.ComponentType_COMPONENT_TYPE_METAL_IMAGE_CACHE_SYNC,
+		StartedAt:     time.Now(),
+		Version: apiv2.Version{
+			Version:   v.Version,
+			Revision:  v.Revision,
+			GitSha1:   v.GitSHA1,
+			BuildDate: v.BuildDate,
+		},
+	})
 
 	log.Info("metal-console", "version", v.V.String(), "port", spec.Port, "metal-apiserver", spec.MetalAPIServerURL, "devmode", spec.DevMode())
 	if err := console.NewServer(log, spec, client).Run(); err != nil {
